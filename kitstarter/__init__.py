89e5e383f3fd47604bf597c6c962d51d17232a14
Zen Master SoSo <zen_master_soso@dridesign.sh.cn>
2026-07-03 18:35:23 +0800

    Bump version 0.5.1 -> 0.6.0

--- kitstarter/__init__.py
+++ kitstarter/__init__.py
@@ -38,7 +38,7 @@ from PyQt5.QtWidgets import QApplication, QWidget, QSplitter
 from qt_extras import DevilBox
 from conn_jack import JackConnectError
 
-__version__ = "0.5.1"
+__version__ = "0.6.0"
 
 
 APPLICATION_NAME	= 'KitStarter'
